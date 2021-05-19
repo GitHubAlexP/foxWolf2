95a796453688dab62c3140a70b3f79ef01b30fba
GitHubAlexP <alejandropoupardh@gmail.com>
2021-05-18 20:44:10 -0700

    refactored FileCreating.java
    This is some comment

--- FileCreating.java
+++ FileCreating.java
@@ -4,18 +4,22 @@ public class FileCreating {
 
     public static void main(String[] args) {
 
+        // SCRIPT REFACTORED IN GITHUB
+        
         // CREATE A FILE
-        try {
+        try
+        {
             File myFile = new File("filename.md");
             if (myFile.createNewFile())
                 System.out.println("File created: " + myFile.getName());
             else
                 System.out.println("File already exists.");
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             System.out.println("An error ocurred.");
             e.printStackTrace();
         }
-
+        
     }
-
 }
